2eb85ce13d2d90156ce76a3dc4f832dcf66897de
wesleyjohnson <wesleyjohnson@5da24444-3c17-0410-b9f4-a1bbb4243164>
2014-07-08 20:05:54 +0000

    w100 sight lines ffloor

--- legacy_one/trunk/src/p_sight.c
+++ legacy_one/trunk/src/p_sight.c
@@ -54,6 +54,7 @@ fixed_t         see_bottomslope;   // slopes to top and bottom of target
 
 // Check Sight internal global vars
 divline_t       cs_trace;        // from t1 to t2
+subsector_t *   cs_t2_subsector; // location of t2
 fixed_t         cs_t2x, cs_t2y;
 fixed_t         cs_startz;       // eye z of looker
 
@@ -111,9 +112,7 @@ static int P_DivlineSide( fixed_t x, fixed_t y, divline_t* node )
 
 //
 // P_InterceptVector2
-// Returns the fractional intercept point
-// along the first divline.
-// This is only called by the addthings and addlines traversers.
+// Returns the fractional intercept point along the first divline.
 //
 static fixed_t P_InterceptVector2( divline_t* v2, divline_t* v1 )
 {
@@ -134,6 +133,105 @@ static fixed_t P_InterceptVector2( divline_t* v2, divline_t* v1 )
     return frac;
 }
 
+
+static fixed_t  prev_frac;  // sight trace frac from last linedef crossing
+
+// Clip the sight line with the solid 3dfloors.
+// At the position along the cs_trace sight line, at frac.
+static void intercept_ffloor( subsector_t * ssec, fixed_t frac )
+{
+    fixed_t  t_slope, b_slope;
+    fixed_t  clip_frac_t, clip_frac_b;
+    fixed_t  th, bh;
+    ffloor_t * ff;  // fake floor
+
+    // This was not present for any previous version.
+    if( demoversion < 145 )  return;
+
+    // This ignores blocking floors that are not solid, and only maintains
+    // one sight gap through the 3d floors.
+    // Lesser sight gaps are treated as blocked.
+    // It will clip the top and bottom sight lines.
+    for( ff = ssec->sector->ffloors; ff; ff = ff->next)
+    {
+        if( !(ff->flags & FF_SOLID) )  continue;
+
+        th = *ff->topheight;
+        bh = *ff->bottomheight;
+
+        if( cs_startz > th )
+        {
+	   // look down on floor
+	   clip_frac_t = frac;
+	   clip_frac_b = prev_frac;
+	}
+        else if ( cs_startz < bh )
+        {
+	   // look up at floor
+	   clip_frac_t = prev_frac;
+	   clip_frac_b = frac;
+        }
+        else
+        {
+	   // edge of floor at eye height
+	   clip_frac_t = prev_frac;
+	   clip_frac_b = prev_frac;
+        }
+        // Quick rejects.
+        if( FixedMul( see_bottomslope, clip_frac_t ) > (th - cs_startz) )
+	   continue;  // bottom sight is above highest part of floor
+        if( FixedMul( see_topslope, clip_frac_b ) < (bh - cs_startz) )
+	   continue;  // top sight is below lowest part of floor
+       
+        // Compare the occlusion slopes to the sight line slopes.
+        t_slope = FixedDiv (th - cs_startz , clip_frac_t);
+	b_slope = FixedDiv (bh - cs_startz , clip_frac_b);
+
+        // Assume t_slope > b_slope.
+	// Assume see_topslope > see_bottomslope.
+	// Assume b_slope < see_topslope and t_slope > see_bottomslope.
+        if( b_slope <= see_bottomslope )
+        {
+	    if( t_slope >= see_topslope )
+	    {
+	        // This occurs most often, so make it first test.
+	        see_bottomslope = FIXED_MAX; // total occlusion
+	        see_topslope = FIXED_MIN;
+	        break;
+	    }
+	    // (t_slope < see_topslope)
+	    see_bottomslope = t_slope;  // th clips bottom sight
+        }
+        else
+        {
+	    // (b_slope > see_bottomslope).
+	    // If this is simplified there will be overflow math errors in
+	    // (see_topslope - t_slope) when near the viewer (t_slope -> FIXED_MAX).
+	    if( t_slope >= see_topslope )
+	    {
+	        see_topslope = b_slope;  // bh clips top sight
+	    }
+	    else
+	    {
+	        // The 3dfloor is in the middle of the sight lines.
+	        // Trying to clip both top and bottom will loose everything.
+	        // Keep only the largest gap in the sight lines.
+	        // Block the 3dfloor and lose the other sight gap.
+	        if( (see_topslope - t_slope) > (b_slope - see_bottomslope) )
+	        {
+		    see_bottomslope = t_slope;  // th clips bottom sight
+		}
+	        else if( b_slope < see_topslope )
+	        {
+		    see_topslope = b_slope;  // bh clips top sight
+	        }
+	    }
+        }
+        if (see_topslope <= see_bottomslope)  break;  // cannot see
+    }
+}
+
+
 //
 // P_CrossSubsector
 // Returns true
@@ -143,18 +241,16 @@ static boolean P_CrossSubsector (int num)
 {
     seg_t*              seg;
     line_t*             line;
-    int                 s1;
-    int                 s2;
+    int                 s1, s2;
     int                 count;
     subsector_t*        sub;
     sector_t*           front;
     sector_t*           back;
-    fixed_t             opentop;
-    fixed_t             openbottom;
+    fixed_t             opentop, openbottom;
     divline_t           divl;
     vertex_t*           v1;
     vertex_t*           v2;
-    fixed_t             frac;
+    fixed_t             frac;  // 1.0
     fixed_t             slope;
 
 #ifdef RANGECHECK
@@ -164,6 +260,20 @@ static boolean P_CrossSubsector (int num)
 #endif
 
     sub = &subsectors[num];
+    if( sub == cs_t2_subsector )
+    {
+        // This is the subsector that t2 is within, so there cannot be
+	// any new linedef crossings.
+        if( sub->sector->ffloors )
+        {
+	    // Clip for the 3dfloors.
+	    intercept_ffloor( sub, FRACUNIT );
+
+	    if (see_topslope <= see_bottomslope)
+	       return false;               // stop
+	}
+        return true;
+    }
 
     // check lines
     count = sub->numlines;
@@ -173,20 +283,21 @@ static boolean P_CrossSubsector (int num)
     {
         line = seg->linedef;
 
-        // allready checked other side?
+        // already checked other side?
         if (line->validcount == validcount)
             continue;
 
         line->validcount = validcount;
 
+        // Check the vertex of the line segment against the sight trace.
         v1 = line->v1;
         v2 = line->v2;
-        s1 = P_DivlineSide (v1->x,v1->y, &cs_trace);
+        s1 = P_DivlineSide (v1->x, v1->y, &cs_trace);
         s2 = P_DivlineSide (v2->x, v2->y, &cs_trace);
 
         // line isn't crossed?
         if (s1 == s2)
-            continue;
+            continue;  // both vertex of the linedef are on same side of cs_trace
 
         divl.x = v1->x;
         divl.y = v1->y;
@@ -197,7 +308,7 @@ static boolean P_CrossSubsector (int num)
 
         // line isn't crossed?
         if (s1 == s2)
-            continue;
+            continue; // both vertex of the trace are on same side of the linedef
 
         // stop because it is not two sided anyway
         // might do this after updating validcount?
@@ -220,7 +331,7 @@ static boolean P_CrossSubsector (int num)
         else
             opentop = back->ceilingheight;
 
-        // because of ceiling height differences
+        // because of floor height differences
         if (front->floorheight > back->floorheight)
             openbottom = front->floorheight;
         else
@@ -230,6 +341,8 @@ static boolean P_CrossSubsector (int num)
         if (openbottom >= opentop)
             return false;               // stop
 
+        // Test the sight lines across this linedef segment.
+        // Fraction of the sight trace covered.
         frac = P_InterceptVector2 (&cs_trace, &divl);
 
         if (front->floorheight != back->floorheight)
@@ -248,7 +361,18 @@ static boolean P_CrossSubsector (int num)
 
         if (see_topslope <= see_bottomslope)
             return false;               // stop
+
+        if( sub->sector->ffloors )
+        {
+	    // Clip for the 3dfloors.
+	    intercept_ffloor( sub, frac );
+
+	    if (see_topslope <= see_bottomslope)
+	       return false;               // stop
+	}
+        prev_frac = frac;
     }
+
     // passed the subsector ok
     return true;
 }
@@ -285,7 +409,7 @@ static boolean P_CrossBSPNode (int bspnum)
         return false;
 
     // the partition plane is crossed here
-    if (side == P_DivlineSide (cs_t2x, cs_t2y,(divline_t *)bsp))
+    if (side == P_DivlineSide (cs_t2x, cs_t2y, (divline_t *)bsp))
     {
         // the line doesn't touch the other side
         return true;
@@ -304,8 +428,7 @@ static boolean P_CrossBSPNode (int bspnum)
 //
 boolean P_CheckSight( mobj_t* t1, mobj_t* t2 )
 {
-    int         s1;
-    int         s2;
+    int         s1, s2;
     int         pnum;
     int         bytenum;
     int         bitnum;
@@ -346,9 +469,11 @@ boolean P_CheckSight( mobj_t* t1, mobj_t* t2 )
 
     validcount++;
 
-    cs_startz = t1->z + t1->height - (t1->height>>2);
-    see_topslope = (t2->z+t2->height) - cs_startz;
-    see_bottomslope = (t2->z) - cs_startz;
+    cs_startz = t1->z + t1->height - (t1->height>>2);  // eyes at 3/4
+    // Slope is (height / horz.), where horz. is measured such that the
+    // the distance from eyes to target = 1.  Slope here is (height/1).
+    see_topslope = (t2->z + t2->height) - cs_startz;   // head of target
+    see_bottomslope = (t2->z) - cs_startz;             // feet of target
 
     cs_trace.x = t1->x;
     cs_trace.y = t1->y;
@@ -356,6 +481,10 @@ boolean P_CheckSight( mobj_t* t1, mobj_t* t2 )
     cs_t2y = t2->y;
     cs_trace.dx = t2->x - t1->x;
     cs_trace.dy = t2->y - t1->y;
+    cs_t2_subsector = t2->subsector;  // location of t2
+   
+    // Setup for 3dfloor sight tests
+    prev_frac = 0;
 
     // the head node is the last node output
     return P_CrossBSPNode (numnodes-1);
@@ -422,6 +551,9 @@ boolean P_CheckSight2( mobj_t* t1, mobj_t* t2, fixed_t px, fixed_t py, fixed_t p
     cs_trace.dx = px - t1->x;
     cs_trace.dy = py - t1->y;
 
+    // Setup for 3dfloor sight tests
+    prev_frac = 0;
+    
     // the head node is the last node output
     return P_CrossBSPNode (numnodes-1);
 }
