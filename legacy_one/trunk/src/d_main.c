d9e6cfe196a3d14c784249b804c5d990fba769d5
wesleyjohnson <wesleyjohnson@5da24444-3c17-0410-b9f4-a1bbb4243164>
2020-05-09 12:08:20 +0000

    w108_112 ver 1.48.3

--- legacy_one/trunk/src/d_main.c
+++ legacy_one/trunk/src/d_main.c
@@ -276,13 +276,13 @@
 
 // Versioning
 #ifndef SVN_REV
-#define SVN_REV "1525"
+#define SVN_REV "1526"
 #endif
 
 
 // Version number: major.minor.revision
 const int  VERSION  = 148; // major*100 + minor
-const int  REVISION = 2;   // for bugfix releases, should not affect compatibility. has nothing to do with svn revisions.
+const int  REVISION = 3;   // for bugfix releases, should not affect compatibility. has nothing to do with svn revisions.
 static const char VERSIONSTRING[] = "(rev " SVN_REV ")";
 //static const char VERSIONSTRING[] = "beta (rev " SVN_REV ")";
 char VERSION_BANNER[80];
